730931682d3f05a373386918a3d6bfddf7c5bdc8
Wynter Woods <zerotri@gmail.com>
2014-12-15 16:11:57 -0800

    NanoVG graphics test

--- main.c
+++ main.c
@@ -12,8 +12,10 @@
 #include <GLES2/gl2.h>
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
-#include "VG/openvg.h"
-#include "VG/vgu.h"
+#include <VG/openvg.h>
+#include <VG/vgu.h>
+#define NANOVG_GLES2_IMPLEMENTATION
+#include <nanovg.h>
 #include <graphics/fbdev/fbdev.h>
 #include <graphics/canvas/canvas.h>
 #include <graphics/seps114a/seps114a.h>
@@ -28,6 +30,7 @@ static framebuffer_device_s fb;
 static stak_seps114a_s lcd_device;
 static stak_bq27510_device_s gauge_device;
 static volatile sig_atomic_t terminate = 0;
+static NVGcontext* vg = NULL;
 static float rotateN = 0.0f;
 
 #define max(a,b) \
@@ -53,284 +56,53 @@ void term(int signum)
     printf("Terminating...\n");
     terminate = 1;
 }
-typedef struct
-{
-    VGFillRule      m_fillRule;
-    VGPaintMode     m_paintMode;
-    VGCapStyle      m_capStyle;
-    VGJoinStyle     m_joinStyle;
-    float           m_miterLimit;
-    float           m_strokeWidth;
-    VGPaint         m_fillPaint;
-    VGPaint         m_strokePaint;
-    VGPath          m_path;
-} PathData;
-
-typedef struct
-{
-    PathData*           m_paths;
-    int                 m_numPaths;
-} PS;
-
-PS* PS_construct(const char* commands, int commandCount, const float* points, int pointCount)
-{
-    PS* ps = (PS*)malloc(sizeof(PS));
-    int p = 0;
-    int c = 0;
-    int i = 0;
-    int paths = 0;
-    int maxElements = 0;
-    unsigned char* cmd;
-    UNREF(pointCount);
-
-    while(c < commandCount)
-    {
-        int elements, e;
-        c += 4;
-        p += 8;
-        elements = (int)points[p++];
-        assert(elements > 0);
-        if(elements > maxElements)
-            maxElements = elements;
-        for(e=0;e<elements;e++)
-        {
-            switch(commands[c])
-            {
-            case 'M': p += 2; break;
-            case 'L': p += 2; break;
-            case 'C': p += 6; break;
-            case 'E': break;
-            default:
-                assert(0);      //unknown command
-            }
-            c++;
-        }
-        paths++;
-    }
-
-    ps->m_numPaths = paths;
-    ps->m_paths = (PathData*)malloc(paths * sizeof(PathData));
-    cmd = (unsigned char*)malloc(maxElements);
-
-    i = 0;
-    p = 0;
-    c = 0;
-    while(c < commandCount)
-    {
-        int elements, startp, e;
-        float color[4];
-
-        //fill type
-        int paintMode = 0;
-        ps->m_paths[i].m_fillRule = VG_NON_ZERO;
-        switch( commands[c] )
-        {
-        case 'N':
-            break;
-        case 'F':
-            ps->m_paths[i].m_fillRule = VG_NON_ZERO;
-            paintMode |= VG_FILL_PATH;
-            break;
-        case 'E':
-            ps->m_paths[i].m_fillRule = VG_EVEN_ODD;
-            paintMode |= VG_FILL_PATH;
-            break;
-        default:
-            assert(0);      //unknown command
-        }
-        c++;
-
-        //stroke
-        switch( commands[c] )
-        {
-        case 'N':
-            break;
-        case 'S':
-            paintMode |= VG_STROKE_PATH;
-            break;
-        default:
-            assert(0);      //unknown command
-        }
-        ps->m_paths[i].m_paintMode = (VGPaintMode)paintMode;
-        c++;
-
-        //line cap
-        switch( commands[c] )
-        {
-        case 'B':
-            ps->m_paths[i].m_capStyle = VG_CAP_BUTT;
-            break;
-        case 'R':
-            ps->m_paths[i].m_capStyle = VG_CAP_ROUND;
-            break;
-        case 'S':
-            ps->m_paths[i].m_capStyle = VG_CAP_SQUARE;
-            break;
-        default:
-            assert(0);      //unknown command
-        }
-        c++;
-
-        //line join
-        switch( commands[c] )
-        {
-        case 'M':
-            ps->m_paths[i].m_joinStyle = VG_JOIN_MITER;
-            break;
-        case 'R':
-            ps->m_paths[i].m_joinStyle = VG_JOIN_ROUND;
-            break;
-        case 'B':
-            ps->m_paths[i].m_joinStyle = VG_JOIN_BEVEL;
-            break;
-        default:
-            assert(0);      //unknown command
-        }
-        c++;
-
-        //the rest of stroke attributes
-        ps->m_paths[i].m_miterLimit = points[p++];
-        ps->m_paths[i].m_strokeWidth = points[p++];
-
-        //paints
-        color[0] = points[p++];
-        color[1] = points[p++];
-        color[2] = points[p++];
-        color[3] = 1.0f;
-        ps->m_paths[i].m_strokePaint = vgCreatePaint();
-        vgSetParameteri(ps->m_paths[i].m_strokePaint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
-        vgSetParameterfv(ps->m_paths[i].m_strokePaint, VG_PAINT_COLOR, 4, color);
-
-        color[0] = points[p++];
-        color[1] = points[p++];
-        color[2] = points[p++];
-        color[3] = 1.0f;
-        ps->m_paths[i].m_fillPaint = vgCreatePaint();
-        vgSetParameteri(ps->m_paths[i].m_fillPaint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
-        vgSetParameterfv(ps->m_paths[i].m_fillPaint, VG_PAINT_COLOR, 4, color);
-
-        //read number of elements
-
-        elements = (int)points[p++];
-        assert(elements > 0);
-        startp = p;
-        for(e=0;e<elements;e++)
-        {
-            switch( commands[c] )
-            {
-            case 'M':
-                cmd[e] = VG_MOVE_TO | VG_ABSOLUTE;
-                p += 2;
-                break;
-            case 'L':
-                cmd[e] = VG_LINE_TO | VG_ABSOLUTE;
-                p += 2;
-                break;
-            case 'C':
-                cmd[e] = VG_CUBIC_TO | VG_ABSOLUTE;
-                p += 6;
-                break;
-            case 'E':
-                cmd[e] = VG_CLOSE_PATH;
-                break;
-            default:
-                assert(0);      //unknown command
-            }
-            c++;
-        }
-
-        ps->m_paths[i].m_path = vgCreatePath(VG_PATH_FORMAT_STANDARD, VG_PATH_DATATYPE_F, 1.0f, 0.0f, 0, 0, (unsigned int)VG_PATH_CAPABILITY_ALL);
-        vgAppendPathData(ps->m_paths[i].m_path, elements, cmd, points + startp);
-        i++;
-    }
-    free(cmd);
-    return ps;
-}
-
-void PS_destruct(PS* ps)
-{
-    int i;
-    assert(ps);
-    for(i=0;i<ps->m_numPaths;i++)
-    {
-        vgDestroyPaint(ps->m_paths[i].m_fillPaint);
-        vgDestroyPaint(ps->m_paths[i].m_strokePaint);
-        vgDestroyPath(ps->m_paths[i].m_path);
-    }
-    free(ps->m_paths);
-    free(ps);
-}
-
-void PS_render(PS* ps)
-{
-    int i;
-    //assert(ps);
-    vgSeti(VG_BLEND_MODE, VG_BLEND_SRC_OVER);
-
-    for(i=0;i<ps->m_numPaths;i++)
-    {
-        vgSeti(VG_FILL_RULE, ps->m_paths[i].m_fillRule);
-        vgSetPaint(ps->m_paths[i].m_fillPaint, VG_FILL_PATH);
-
-        if(ps->m_paths[i].m_paintMode & VG_STROKE_PATH)
-        {
-            vgSetf(VG_STROKE_LINE_WIDTH, ps->m_paths[i].m_strokeWidth);
-            vgSeti(VG_STROKE_CAP_STYLE, ps->m_paths[i].m_capStyle);
-            vgSeti(VG_STROKE_JOIN_STYLE, ps->m_paths[i].m_joinStyle);
-            vgSetf(VG_STROKE_MITER_LIMIT, ps->m_paths[i].m_miterLimit);
-            vgSetPaint(ps->m_paths[i].m_strokePaint, VG_STROKE_PATH);
-        }
-
-        vgDrawPath(ps->m_paths[i].m_path, ps->m_paths[i].m_paintMode);
-    }
-    //assert(vgGetError() == VG_NO_ERROR);
-}
-
-PS* tiger = NULL;
 
 /*--------------------------------------------------------------*/
 
 void render(int w, int h)
 {
-        float clearColor[4] = {1,1,1,1};
-        float scale = w / (tigerMaxX - tigerMinX);
-        rotateN += 1.0f;
-
-        vgSetfv(VG_CLEAR_COLOR, 4, clearColor);
-        vgClear(0, 0, w, h);
-
-        vgLoadIdentity();
-        vgTranslate(w*0.5f, h*0.5f);
-        vgRotate(rotateN);
-        vgTranslate(-w*0.5f, -h*0.5f);
-        vgScale(scale, scale);
-        vgTranslate(-tigerMinX, -tigerMinY + 0.5f * (h / scale - (tigerMaxY - tigerMinY)));
-
-        PS_render(tiger);
-        //assert(vgGetError() == VG_NO_ERROR);
-
+    float clearColor[4] = {1,1,1,1};
+    nvgResetTransform(vg);
+    nvgFillColor(vg, nvgRGBA(28,30,34,192));
+    nvgStrokeColor(vg, nvgRGBA(0,0,0,32));
+    nvgCircle(vg, w*0.5f, h*0.5f, 30.5f);
 }
 
 /*--------------------------------------------------------------*/
 
 void init()
 {
-    tiger = PS_construct(tigerCommands, tigerCommandCount, tigerPoints, tigerPointCount);
+    vg = nvgCreateGLES2(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
+    if (vg == NULL) {
+        printf("Could not init nanovg.\n");
+        return -1;
+    }
+    glClearColor(1,1,1,1);
 }
 
 /*--------------------------------------------------------------*/
 
 void deinit(void)
 {
-    PS_destruct(tiger);
+    nvgDeleteGLES2(vg);
 }
 
 int redraw()
 {
     // start with a clear screen
-    glClear( GL_COLOR_BUFFER_BIT );
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+
+    nvgBeginFrame(vg, winWidth, winHeight, pxRatio);
 
     render(canvas.screen_width,canvas.screen_height);
+
+    glEnable(GL_DEPTH_TEST);
+
     stak_canvas_swap(&canvas);
     stak_canvas_copy(&canvas, (char*)lcd_device.framebuffer, 96 * 2);
     return 0;
