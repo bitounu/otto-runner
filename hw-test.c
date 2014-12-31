70e9a5a6c4047229d4fb091f7f9806774d350185
Wynter Woods <zerotri@gmail.com>
2014-12-30 21:24:13 -0800

    Some hardware test changes. Need to move them to the new folder structure.

--- hw-test.c
+++ hw-test.c
@@ -29,24 +29,6 @@
 void init();
 void shutdown();
 void redraw();
-void update_pin_states();
-void* update_encoder(void* arg);
-void* update_display(void* arg);
-
-int get_shutter_pressed();
-int get_shutter_released();
-int get_rotary_pressed();
-int get_rotary_released();
-int get_rotary_position();
-
-void shutter_test();
-void rotary_left_test();
-void rotary_right_test();
-void rotary_switch_test();
-void camera_test_a();
-void camera_test_b();
-void finished_tests();
-
 
 // helper macros
 #define ZERO_OBJECT(_object) memset( &_object, 0, sizeof( _object ) )
@@ -58,41 +40,11 @@ static framebuffer_device_s fb;
 static stak_seps114a_s lcd_device;
 static stak_bq27510_device_s gauge_device;
 static volatile sig_atomic_t terminate = 0;
-pthread_t thread_rotary_poll, thread_seps114a_update;
 
 // gpio settings and state
-const int pin_shutter = 23;
-const int pin_rotary_button = 17;
-const int pin_rotary_a = 15;
-const int pin_rotary_b = 14;
-const int pin_pwm = 18;
-
-int value_shutter = LOW;
-int value_rotary_left = LOW;
-int value_rotary_right = LOW;
-int shutter_state = 1;
-int rotary_switch_State = 1;
-volatile int last_encoded_value = 0, encoder_value = 0;
-int testing_position = 0;
-
-
-// simple test framework
-void run_test();
-void next_test();
-
-int current_test = 0;
-
-typedef void ( *test_fptr_t )( void );
-
-test_fptr_t tests[] = {
-	shutter_test,
-	rotary_left_test,
-	rotary_right_test,
-	rotary_switch_test,
-	camera_test_a,
-	camera_test_b,
-	finished_tests
-};
+const int pin_power = 4;
+const int pin_shutter = 16;
+const int pin_pwm = 18;				// 18
 
 #define max(a,b) \
    ({ __typeof__ (a) _a = (a); \
@@ -118,13 +70,11 @@ typedef struct framerate_lock {
 
 
 const char* files[] = {
-	"/root/assets/shutter.png",
-	"/root/assets/rotleft.png",
-	"/root/assets/rotright.png",
-	"/root/assets/rotswitch.png",
-	"/root/assets/camera.png",
-	"/root/assets/oled.png",
-	//"assets/complete.png"
+	"assets/shutter.png",
+	"assets/camera.png",
+	"assets/rotleft.png",
+	"assets/rotright.png",
+	"assets/rotswitch.png"
 };
 
 typedef struct image_s{
@@ -169,9 +119,11 @@ int main(int argc, char** argv) {
             last_time = current_time;
         }
 
-
-		update_pin_states();
-		redraw();
+		uint8_t falling = bcm2835_gpio_lev( pin_power );
+		if(!falling) {
+			printf("low\n");
+		}
+		//redraw();
 
         delta_time = (get_time() - current_time);
         uint64_t sleep_time = min(33000000L, 33000000L - max(0,delta_time));
@@ -195,15 +147,12 @@ void init() {
 		printf("Failed to init BCM2835 library.\n");
 		return;
 	}
-	bcm2835_gpio_fsel(pin_rotary_button, BCM2835_GPIO_FSEL_INPT);
-	bcm2835_gpio_fsel(pin_rotary_a, BCM2835_GPIO_FSEL_INPT);
-	bcm2835_gpio_fsel(pin_rotary_b, BCM2835_GPIO_FSEL_INPT);
 	bcm2835_gpio_fsel(pin_pwm, BCM2835_GPIO_FSEL_ALT5);
-	bcm2835_gpio_fsel(23, BCM2835_GPIO_FSEL_INPT);
-	bcm2835_gpio_set_pud(23, BCM2835_GPIO_PUD_UP);
-	bcm2835_gpio_set_pud(pin_rotary_button, BCM2835_GPIO_PUD_UP);
-	bcm2835_gpio_set_pud(pin_rotary_a, BCM2835_GPIO_PUD_UP);
-	bcm2835_gpio_set_pud(pin_rotary_b, BCM2835_GPIO_PUD_UP);
+	bcm2835_gpio_fsel(pin_shutter, BCM2835_GPIO_FSEL_INPT);
+	bcm2835_gpio_fsel(pin_power, BCM2835_GPIO_FSEL_INPT);
+    //bcm2835_gpio_set_pud(pin_power, BCM2835_GPIO_PUD_UP);
+	bcm2835_gpio_fen(pin_shutter);
+	//bcm2835_gpio_set_pud(pin_shutter, BCM2835_GPIO_PUD_UP);
 	bcm2835_pwm_set_clock(BCM2835_PWM_CLOCK_DIVIDER_16);
 	bcm2835_pwm_set_mode(0, 1, 1);
 	bcm2835_pwm_set_range(0, 1000);
@@ -222,15 +171,14 @@ void init() {
 	stak_seps114a_init(&lcd_device);
 	stak_canvas_create(&canvas, STAK_CANVAS_OFFSCREEN, 96, 96);
 
-	// setup bq27510 gas gauge
-    int gauge_error = stak_bq27510_open("/dev/i2c-1", &gauge_device);
+	int gauge_error = stak_bq27510_open("/dev/i2c-1", &gauge_device);
     stak_bq27510_close(&gauge_device);
 
     if(gauge_error != 0) {
     	int x, y;
     	for(y = 0; y < 96; y++) {
 			for(x = 0; x < 96; x++) {
-				((uint16_t*)lcd_device.framebuffer)[y * 96 + x] = rgb24_to_16(0x00ff0000);
+				((uint16_t*)lcd_device.framebuffer)[y * 96 + x] = rgb24_to_16(0xff000000);
 			}
 		}
 		stak_seps114a_update(&lcd_device);
@@ -247,21 +195,11 @@ void init() {
 			}
 		}
     }
-	pthread_create(&thread_rotary_poll, NULL, update_encoder, NULL);
-	//pthread_create(&thread_seps114a_update, NULL, update_display, NULL);
-	
-	printf("Please press shutter button.\n");
+
 }
 
 void shutdown() {
-	if(pthread_join(thread_rotary_poll, NULL)) {
-		fprintf(stderr, "Error joining thread\n");
-		return;
-	}
-	/*if(pthread_join(thread_seps114a_update, NULL)) {
-		fprintf(stderr, "Error joining thread\n");
-		return;
-	}*/
+	bcm2835_pwm_set_data(0, 0);
 	stak_canvas_destroy(&canvas);
 	stak_seps114a_close(&lcd_device);
 	int cur_image = 0;
@@ -270,35 +208,10 @@ void shutdown() {
 	}
 }
 
-void* update_display(void* arg) {
-
-    uint64_t last_time, current_time, start_time, delta_time;
-    delta_time = start_time = last_time = current_time = get_time();
-    int frames_this_second = 0;
-    int frames_per_second = 0;
-	
-	while(!terminate) {
-
-		/*frames_this_second++;
-        current_time = get_time();
-
-        
-        if(current_time > last_time + 1000000) {
-            frames_per_second = frames_this_second;
-            frames_this_second = 0;
-            last_time = current_time;
-        }
-		stak_seps114a_update(&lcd_device);
-        delta_time = (get_time() - current_time);
-        uint64_t sleep_time = min(16000000L, 16000000L - max(0,delta_time));
-        nanosleep((struct timespec[]){{0, sleep_time}}, NULL);*/
-        nanosleep((struct timespec[]){{0, 333000000L}}, NULL);
-	}
-	return 0;
-}
-
 void draw_image(int image) {
 	int x, y;
+	if(images[image].data == 0)
+		return;
 	for(y = 0; y < 96; y++) {
 		for(x = 0; x < 96; x++) {
 			((uint16_t*)lcd_device.framebuffer)[y * 96 + x] = rgb24_to_16(( (uint32_t*) images[image].data)[y * images[image].w + x]);
@@ -309,123 +222,13 @@ void redraw() {
 	// start with a clear screen
 	glClear( GL_COLOR_BUFFER_BIT );
 
+	uint8_t falling = bcm2835_gpio_lev( pin_power );
+
 	//render(canvas.screen_width,canvas.screen_height);
-	run_test();
+	
+	draw_image( falling );
+	//run_test();
 	//stak_canvas_swap(&canvas);
 	//stak_canvas_copy(&canvas, (char*)lcd_device.framebuffer, 96 * 2);
 	stak_seps114a_update(&lcd_device);
-}
-
-void* update_encoder(void* arg) {
-	uint64_t last_time, current_time, start_time, delta_time;
-    delta_time = start_time = last_time = current_time = get_time();
-    int frames_this_second = 0;
-    int frames_per_second = 0;
-	const int encoding_matrix[4][4] = {
-		{ 0,-1, 1, 0},
-		{ 1, 0, 0,-1},
-		{-1, 0, 0, 1},
-		{ 0, 1,-1, 0}
-	};
-
-	while(!terminate) {
-		int encoded = (bcm2835_gpio_lev(pin_rotary_a) << 1)
-					 | bcm2835_gpio_lev(pin_rotary_b);
-
-		int change = encoding_matrix[last_encoded_value][encoded];
-		encoder_value += change;
-		//if(change != 0)
-		//	bcm2835_pwm_set_data(0, (1024/32)*encoder_value);
-		last_encoded_value = encoded;
-
-		frames_this_second++;
-        current_time = get_time();
-
-        
-        if(current_time > last_time + 1000000) {
-            frames_per_second = frames_this_second;
-            frames_this_second = 0;
-            last_time = current_time;
-            //printf("SEPS114A FPS: %i\n", frames_per_second);
-        }
-        delta_time = (get_time() - current_time);
-        uint64_t sleep_time = min(16000000L, 16000000L - max(0,delta_time));
-        nanosleep((struct timespec[]){{0, sleep_time}}, NULL);
-	}
-	return 0;
-}
-
-void run_test() {
-	tests[current_test]();
-}
-
-void next_test() {
-	bcm2835_pwm_set_data(0, (1024/32)*12);
-	nanosleep((struct timespec[]){{0, 500000000L}}, NULL);
-	bcm2835_pwm_set_data(0, 0);
-	if(current_test < ARRAY_COUNT(tests))
-		current_test++;
-}
-
-void update_pin_states() {
-
-	if( bcm2835_gpio_lev(pin_shutter) != shutter_state ){
-		shutter_state = !shutter_state;
-	}
-
-	if( bcm2835_gpio_lev(pin_rotary_button) != rotary_switch_State ){
-		rotary_switch_State = !rotary_switch_State;
-	}
-}
-
-int get_shutter_pressed() {
-	return (shutter_state == LOW);
-}
-int get_rotary_pressed() {
-	return (rotary_switch_State == HIGH);
-}
-int get_rotary_position() {
-	return encoder_value;
-}
-
-void shutter_test() {
-	draw_image(0);
-	if(get_shutter_pressed()) {
-		next_test();
-		testing_position = encoder_value;
-	}
-}
-void rotary_left_test() {
-	draw_image(1);
-	if(encoder_value < testing_position - 15) {
-		next_test();
-		testing_position = encoder_value;
-	}
-}
-void rotary_right_test() {
-	draw_image(2);
-	if(encoder_value > testing_position + 15) {
-		next_test();
-	}
-}
-void rotary_switch_test() {
-	draw_image(3);
-	if(get_rotary_pressed()) {
-		next_test();
-	}
-}
-void camera_test_a() {
-	draw_image(4);
-	stak_seps114a_update(&lcd_device);
-	nanosleep((struct timespec[]){{3, 0}}, NULL);
-	next_test();
-}
-void camera_test_b() {
-	draw_image(5);
-	if(get_shutter_pressed()) {
-		next_test();
-	}
-}
-void finished_tests() {
-	terminate = 1;
 }
