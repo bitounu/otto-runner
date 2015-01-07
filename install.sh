1c5308619a28cc461fb506794458ce4f4264b76d
Wynter Woods <zerotri@gmail.com>
2015-01-07 17:25:24 +0000

    add wifi

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 cp python-init /etc/init.d/python
 cp otto-demo-init /etc/init.d/otto-demo
 cp fastcamd-init /etc/init.d/fastcamd
-
+cp wifi-init /etc/init.d/wifi
 update-rc.d python defaults
+update-rc.d wifi defaults
 update-rc.d otto-demo defaults
 update-rc.d fastcamd defaults
 cp -r fastcmd/ ~/fastcmd
