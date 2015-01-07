644505af8e288f67c176e4a9004d751669480110
Wynter Woods <zerotri@gmail.com>
2015-01-07 16:47:59 +0000

    Install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp python-init /etc/init.d/python
+cp otto-demo-init /etc/init.d/otto-demo
+cp fastcamd-init /etc/init.d/fastcamd
+
+update-rc.d python defaults
+update-rc.d otto-demo defaults
+update-rc.d fastcamd defaults
