d712c3e65697daa90bbeaf8f416523ac37981bb1
Wynter Woods <zerotri@gmail.com>
2015-01-12 21:27:10 +0000

    Modified wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,7 +1,7 @@
 wget http://www.daveconroy.com/wp3/wp-content/uploads/2013/07/hostapd.zip
-unzip hostapd.zip 
+unzip hostapd.zip
 sudo mv /usr/sbin/hostapd /usr/sbin/hostapd.bak
-sudo mv hostapd /usr/sbin/hostapd.edimax 
-sudo ln -sf /usr/sbin/hostapd.edimax /usr/sbin/hostapd 
-sudo chown root.root /usr/sbin/hostapd 
+sudo mv hostapd /usr/sbin/hostapd.edimax
+sudo ln -sf /usr/sbin/hostapd.edimax /usr/sbin/hostapd
+sudo chown root.root /usr/sbin/hostapd
 sudo chmod 755 /usr/sbin/hostapd
