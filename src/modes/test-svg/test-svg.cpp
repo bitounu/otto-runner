3c5d8f57132995137029c44a3bffbf48bc199363
Ryan Alexander <scloopy@gmail.com>
2015-01-21 23:50:37 +0000

    Support solid strokes and fills

--- src/modes/test-svg/test-svg.cpp
+++ src/modes/test-svg/test-svg.cpp
@@ -28,19 +28,54 @@ int shutdown() {
   return 0;
 }
 
+static VGPaint createPaintFromNSVGpaint(const NSVGpaint &svgPaint, float opacity = 1.0f) {
+  auto paint = vgCreatePaint();
+
+  if (svgPaint.type == NSVG_PAINT_COLOR) {
+    float color[] = {
+      (svgPaint.color         & 0xff) / 255.0f,
+      ((svgPaint.color >> 8)  & 0xff) / 255.0f,
+      ((svgPaint.color >> 16) & 0xff) / 255.0f,
+      1.0f
+    };
+    vgSetParameteri(paint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
+    vgSetParameterfv(paint, VG_PAINT_COLOR, 4, color);
+  }
+
+  return paint;
+}
+
+static void strokePaint(const NSVGpaint &svgPaint, float opacity = 1.0f) {
+  auto paint = createPaintFromNSVGpaint(svgPaint, opacity);
+  vgSetPaint(paint, VG_STROKE_PATH);
+  vgDestroyPaint(paint);
+}
+
+static void fillPaint(const NSVGpaint &svgPaint, float opacity = 1.0f) {
+  auto paint = createPaintFromNSVGpaint(svgPaint, opacity);
+  vgSetPaint(paint, VG_FILL_PATH);
+  vgDestroyPaint(paint);
+}
+
 static void strokeColor(float r, float g, float b, float a = 1.0f) {
   VGfloat color[] = { r, g, b, a };
-  VGPaint strokePaint = vgCreatePaint();
-  vgSetParameteri(strokePaint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
-  vgSetParameterfv(strokePaint, VG_PAINT_COLOR, 4, color);
-  vgSetPaint(strokePaint, VG_STROKE_PATH);
-  vgDestroyPaint(strokePaint);
+  VGPaint paint = vgCreatePaint();
+  vgSetParameteri(paint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
+  vgSetParameterfv(paint, VG_PAINT_COLOR, 4, color);
+  vgSetPaint(paint, VG_STROKE_PATH);
+  vgDestroyPaint(paint);
 }
 
 static void strokeWidth(VGfloat width) {
   vgSetf(VG_STROKE_LINE_WIDTH, width);
-  vgSeti(VG_STROKE_CAP_STYLE, VG_CAP_BUTT);
-  vgSeti(VG_STROKE_JOIN_STYLE, VG_JOIN_MITER);
+}
+
+static void strokeCap(VGCapStyle cap) {
+  vgSeti(VG_STROKE_CAP_STYLE, cap);
+}
+
+static void strokeJoin(VGJoinStyle join) {
+  vgSeti(VG_STROKE_JOIN_STYLE, join);
 }
 
 static void moveTo(VGPath path, float x, float y) {
@@ -71,23 +106,33 @@ int update() {
   vgRotate(rotation);
   vgTranslate(-48, -48);
 
-  strokeColor(1.0f, 0.0f, 0.0f);
-  strokeWidth(2.0f);
-
   for (auto shape = image->shapes; shape != NULL; shape = shape->next) {
-    for (auto path = shape->paths; path != NULL; path = path->next) {
-      auto vgPath = vgCreatePath(VG_PATH_FORMAT_STANDARD, VG_PATH_DATATYPE_F, 1.0f, 0.0f, 0, 0, VG_PATH_CAPABILITY_ALL);
+    bool hasStroke = shape->stroke.type != NSVG_PAINT_NONE;
+    bool hasFill = shape->fill.type != NSVG_PAINT_NONE;
 
+    if (hasFill) {
+      fillPaint(shape->fill, shape->opacity);
+    }
+
+    if (hasStroke) {
+      strokeWidth(shape->strokeWidth);
+      strokeJoin(static_cast<VGJoinStyle>(shape->strokeLineJoin));
+      strokeCap(static_cast<VGCapStyle>(shape->strokeLineCap));
+      strokePaint(shape->stroke, shape->opacity);
+    }
+
+    auto vgPath = vgCreatePath(VG_PATH_FORMAT_STANDARD, VG_PATH_DATATYPE_F, 1.0f, 0.0f, 0, 0, VG_PATH_CAPABILITY_ALL);
+
+    for (auto path = shape->paths; path != NULL; path = path->next) {
+      moveTo(vgPath, path->pts[0], path->pts[1]);
       for (int i = 0; i < path->npts - 1; i += 3) {
         float* p = &path->pts[i * 2];
-
-        moveTo(vgPath, p[0], p[1]);
         cubicTo(vgPath, p[2], p[3], p[4], p[5], p[6], p[7]);
       }
-
-      vgDrawPath(vgPath, VG_STROKE_PATH);
-      vgDestroyPath(vgPath);
     }
+
+    vgDrawPath(vgPath, VG_FILL_PATH | VG_STROKE_PATH);
+    vgDestroyPath(vgPath);
   }
 
   return 0;
