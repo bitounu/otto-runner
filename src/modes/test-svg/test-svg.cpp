8d2603d0e05d095d21b04b5cc4ab33e4c5e8eb5b
Ryan Alexander <scloopy@gmail.com>
2015-01-22 00:00:33 +0000

    Respect shape opacity

--- src/modes/test-svg/test-svg.cpp
+++ src/modes/test-svg/test-svg.cpp
@@ -20,7 +20,6 @@ static NSVGimage* image = nullptr;
 
 int init() {
   image = nsvgParseFromFile("src/modes/test-svg/test.svg", "px", 96);
-  printf("size: %f x %f\n", image->width, image->height);
   return 0;
 }
 
@@ -36,7 +35,7 @@ static VGPaint createPaintFromNSVGpaint(const NSVGpaint &svgPaint, float opacity
       (svgPaint.color         & 0xff) / 255.0f,
       ((svgPaint.color >> 8)  & 0xff) / 255.0f,
       ((svgPaint.color >> 16) & 0xff) / 255.0f,
-      1.0f
+      opacity
     };
     vgSetParameteri(paint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
     vgSetParameterfv(paint, VG_PAINT_COLOR, 4, color);
@@ -90,23 +89,8 @@ static void cubicTo(VGPath path, float x1, float y1, float x2, float y2, float x
   vgAppendPathData(path, 1, segs, coords);
 }
 
-int update() {
-  static const float screenWidth  = 96.0f;
-  static const float screenHeight = 96.0f;
-  static const float defaultMatrix[] =
-    { -0.0f, -1.0f, -0.0f, -1.0f,  0.0f,  0.0f, screenWidth, screenHeight, 1.0f };
-
-  static const VGfloat bgColor[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-
-  vgSetfv(VG_CLEAR_COLOR, 4, bgColor);
-  vgClear(0, 0, 96, 96);
-
-  vgLoadMatrix(defaultMatrix);
-  vgTranslate(48, 48);
-  vgRotate(rotation);
-  vgTranslate(-48, -48);
-
-  for (auto shape = image->shapes; shape != NULL; shape = shape->next) {
+static void draw(const NSVGimage &svg) {
+  for (auto shape = svg.shapes; shape != NULL; shape = shape->next) {
     bool hasStroke = shape->stroke.type != NSVG_PAINT_NONE;
     bool hasFill = shape->fill.type != NSVG_PAINT_NONE;
 
@@ -134,11 +118,30 @@ int update() {
     vgDrawPath(vgPath, VG_FILL_PATH | VG_STROKE_PATH);
     vgDestroyPath(vgPath);
   }
+}
+
+int update() {
+  static const float screenWidth  = 96.0f;
+  static const float screenHeight = 96.0f;
+  static const float defaultMatrix[] =
+    { -0.0f, -1.0f, -0.0f, -1.0f,  0.0f,  0.0f, screenWidth, screenHeight, 1.0f };
+
+  static const VGfloat bgColor[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+  vgSetfv(VG_CLEAR_COLOR, 4, bgColor);
+  vgClear(0, 0, 96, 96);
+
+  vgLoadMatrix(defaultMatrix);
+  vgTranslate(48, 48);
+  vgRotate(rotation);
+  vgTranslate(-48, -48);
+
+  draw(*image);
 
   return 0;
 }
 
 int rotary_changed(int delta) {
-  rotation += delta * 2.0f;
+  rotation += delta * 5.0f;
   return 0;
 }
